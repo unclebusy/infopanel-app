243b29cc7889aec7235bbbd4b93f8e5551116845
unclebusy <deniskalagin@gmail.com>
2024-11-16 12:33:05 +0500

    Update project

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,6 +1,6 @@
 import { JobPosition } from './JobPosition';
 import {useDispatch, useSelector} from "react-redux";
-import {selectAllPositions, selectVisiblePositions} from "../store/positions/position-selectors";
+import {selectVisiblePositions} from "../store/positions/position-selectors";
 import {addFilter} from "../store/filters/filter-actions";
 import {selectFilters} from "../store/filters/filter-selectors";
 
